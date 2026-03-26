a27d553f050b750e110eac261690cd62f37c006c
Kirill <boychenkokirill@gmail.com>
2026-03-26 02:15:42 +0000

    refactor: standardize quotation marks in vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.YACT_WEB_PORT ?? '5175'),
+    host: "0.0.0.0",
+    port: parseInt(process.env.YACT_WEB_PORT ?? "5175"),
     strictPort: true,
-    allowedHosts: ['ubuntuserver'],
-  }
+    allowedHosts: ["ubuntuserver"],
+  },
 });
