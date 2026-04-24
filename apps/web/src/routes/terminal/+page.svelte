6b23d60eab085e7babcb9d7934423baa00078b01
bk-bf <boychenkokirill@gmail.com>
2026-04-24 20:35:32 +0000

    feat: add terminal route with TradingTerminalView component and page load function

---
+++ apps/web/src/routes/terminal/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import TradingTerminalView from '$lib/pages/terminal/TradingTerminalView.svelte';
+</script>
+
+<svelte:head>
+	<title>Terminal · YACT</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<TradingTerminalView />
