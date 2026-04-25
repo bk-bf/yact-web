807bd334168a59e018903547ed73cb0ba649399a
bk-bf <boychenkokirill@gmail.com>
2026-04-25 03:47:54 +0000

    feat: update Vite server configuration with host and allowedHosts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
+    host: '0.0.0.0',
     port: parseInt(process.env.YACT_WEB_PORT ?? '5175'),
     strictPort: true,
+    allowedHosts: ['ubuntuserver'],
   }
 });
